6d1d3641ef41b2aefc7aaf972014191b658d02c7
Phillip Berg <phillipb1991@msn.com>
2019-12-13 15:59:12 -0600

    Set up reducer

--- src/index.js
+++ src/index.js
@@ -7,10 +7,27 @@ import {applyMiddleware, createStore, combineReducers} from 'redux';
 import {Provider} from 'react-redux'
 import logger from 'redux-logger';
 
+// Holds feedback
+const feedbackDefault={feeling: '', understanding: '', support: '', comments: ''}
+const feedbackReducer = (state=feedbackDefault, action) =>{
+  switch (action.type) {
+    case "ADD_FEELING":
+      return {...state, feeling:action.payload};
+    case "ADD_UNDERSTANDING":
+      return {...state, understanding:action.payload};
+    case "ADD_SUPPORT":
+      return {...state, support:action.payload};
+    case "ADD_COMMENTS":
+      return {...state, comments:action.payload}
+    default:
+      return state;
+  }
+};
+
 // Set up reducers
 const storeInstance = createStore(
   combineReducers({
-
+    feedbackReducer
   }), applyMiddleware(logger)
 );
 
