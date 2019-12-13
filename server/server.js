7fdac56233d7265a50d95231466f762fffdb7a2d
Phillip Berg <phillipb1991@msn.com>
2019-12-13 16:01:32 -0600

    removed bodyparser

--- server/server.js
+++ server/server.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
 
 /** ---------- MIDDLEWARE ---------- **/
-app.use(bodyParser.json()); // needed for angular requests
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));
 app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
