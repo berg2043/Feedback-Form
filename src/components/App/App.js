9252270062afec7b2a486e2bfa62eab15fa51738
Phillip Berg <phillipb1991@msn.com>
2019-12-13 17:34:18 -0600

    added routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import axios from 'axios';
 import './App.css';
 import { connect } from 'react-redux';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Questions from '../Questions/Questions';
 import Comments from '../Comments/Comments';
 import Review from '../Review/Review';
+import Success from '../Success/Success';
 
 const App = (props)=> {
   return (
@@ -16,11 +16,32 @@ const App = (props)=> {
       </header>
       <br/>
       <Router>
-        <Questions reducerPath="ADD_FEELING" question="How are you feeling today?" placeholder="Feeling?"/>
-        <Questions reducerPath="ADD_UNDERSTANDING" question="How are you understanding the content?" placeholder="Understanding?"/>
-        <Questions reducerPath="ADD_SUPPORT" question="How are you being supported today?" placeholder="Support?"/>
-        <Comments/>
-        <Review/>
+        <Route exact path='/' render={(props)=>
+          <Questions {...props} 
+              reducerPath="ADD_FEELING" 
+              question="How are you feeling today?" 
+              placeholder="Feeling?"
+              routerPath="/understanding"/>}
+        />
+        <Route path='/understanding' render={(props)=>
+          <Questions {...props}
+              reducerPath="ADD_UNDERSTANDING" 
+              question="How are you understanding the content?" 
+              placeholder="Understanding?"
+              routerPath="/support"
+              back="/"/>}
+        />
+        <Route path='/support' render={(props)=>
+          <Questions {...props}
+              reducerPath="ADD_SUPPORT" 
+              question="How are you being supported today?" 
+              placeholder="Support?"
+              routerPath="/comments"
+              back="understanding"/>}
+        />
+        <Route path='/comments' render={(props)=> <Comments {...props}/>}/>
+        <Route path='/review' render={(props)=> <Review {...props}/>}/>
+        <Route path='/thanks' render={(props)=> <Success {...props}/>}/>
       </Router>
     </div>
   );
