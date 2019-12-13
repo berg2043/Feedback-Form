9285b3053cf8ae5cfd2931ca12f95246da2c3ffb
Phillip Berg <phillipb1991@msn.com>
2019-12-13 15:10:56 -0600

    set up base redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import {applyMiddleware, createStore, combineReducers} from 'redux';
+import {Provider} from 'react-redux'
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Set up reducers
+const storeInstance = createStore(
+  combineReducers({
+
+  }), applyMiddleware(logger)
+);
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
