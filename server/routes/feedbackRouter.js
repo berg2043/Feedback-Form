856d5dd8272b541200acfbb55581d42372a3f431
Phillip Berg <phillipb1991@msn.com>
2019-12-14 15:59:06 -0600

    set post get and delete routes

--- server/routes/feedbackRouter.js
+++ server/routes/feedbackRouter.js
@@ -2,6 +2,41 @@ const express = require('express');
 const router = express.Router();
 const pool = require('./../modules/pool')
 
+// GETS feedback from database
+router.get('/', (req,res)=>{
+  const queryText = `SELECT * FROM "feedback" ORDER BY id;`;
+  pool.query(queryText).then(result=>{
+    res.send(result.rows);
+  }).catch(err=>{
+    console.log('ERROR IN POSTING FEEDBACK',err);
+    res.sendStatus(500);
+  });
+});
+
+// DELETES feedback from database
+router.delete('/:id', (req,res)=>{
+  const id = req.params.id
+  const queryText = `DELETE FROM "feedback" WHERE id = $1;`;
+  pool.query(queryText,[id]).then(result=>{
+    res.sendStatus(200);
+  }).catch(err=>{
+    console.log('ERROR IN POSTING FEEDBACK',err);
+    res.sendStatus(500);
+  });
+});
+
+// PUTS feedback on the database
+router.put('/:id', (req,res)=>{
+  const id = req.params.id
+  const queryText = `UPDATE "feedback" SET flagged = true WHERE id = $1;`;
+  pool.query(queryText,[id]).then(result=>{
+    res.sendStatus(200);
+  }).catch(err=>{
+    console.log('ERROR IN POSTING FEEDBACK',err);
+    res.sendStatus(500);
+  });
+});
+
 // POSTS feedback to database
 router.post('/', (req,res)=>{
   console.log(req.body);
